386c260c96948f8c22e9281077549e7b453b16ef
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-10-01 01:35:27 +0530

    Removed timeout and have added a pause for better debugging It will help in taking a full screen shot of the script run Moved WhatsAppKeyExtractNoJavaCheck.bat to obsolete dir as it is no more required with the use of the FindJava.bat script

--- obsolete/WhatsAppKeyExtractNoJavaCheck.bat
+++ obsolete/WhatsAppKeyExtractNoJavaCheck.bat
@@ -1,32 +1,22 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 color 1b
-title WhatsApp Key/DB Extractor v2.2.1
+title WhatsApp Key/DB Extractor v2.2
 echo =========================================================================
 echo = This script will extract the WhatsApp Key file and DB on Android 4.0+ =
 echo = You DO NOT need root for this to work, but you DO need Java installed =
 echo = Base Script by: TripCode                                              =
 echo = Thanks to: Nikolay Elenkov for abe.jar and David Fraser               =
 echo = Updated By: Abinash Bishoyi (Added support for 4.4.X/L devices)       =
-echo = Version: v2.2.1 (1st Oct 2014)                                        =
+echo = Version: v2.2 (15th Aug 2014)                                         =
 echo =========================================================================
-echo.
-
 call FindJava.bat
-if "!JAVA_HOME!" == "" (
-echo Please install Java first and then re-run this bat script.
-@start bin\jxpiinstall.exe
-echo.
-echo Exiting ...
-timeout /t 5
-exit
-) else (
 set "JAVA=!JAVA_HOME!\bin\java.exe"
 "!JAVA!" -version
 echo.
 echo Please connect your Android device with USB Debugging enabled:
 echo.
-)
+
 bin\adb.exe wait-for-device
 bin\adb.exe backup -f tmp\whatsapp.ab -noapk com.whatsapp
 if exist tmp\whatsapp.ab (
@@ -58,4 +48,4 @@ echo Done!
 ) else (
 echo Operation Failed!
 )
-pause
+timeout /t 15
