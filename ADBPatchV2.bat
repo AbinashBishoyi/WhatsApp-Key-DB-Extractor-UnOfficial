0a79e805a7ac7068aea04fa5cc0fc3992ab16acf
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2015-01-25 17:58:20 -0500

    Minor update to ADBPatchV2.bat

--- ADBPatchV2.bat
+++ ADBPatchV2.bat
@@ -1,18 +1,16 @@
 @echo off
-rem title WhatsApp ADB Patch by: TripCode Improvised by: Abinash Bishoyi
 if exist "WhatsApp" (rmdir "WhatsApp" /s /q)
 if exist WhatsApp_Patched.apk del /F WhatsApp_Patched.apk
 if not exist WhatsApp.apk (
 echo Downloading latest WhatsApp
 bin\curl -o WhatsApp.apk http://www.whatsapp.com/android/current/WhatsApp.apk
 )
-echo Decompiling APK
+echo I: Decompiling APK
 java -jar bin\apktool.jar d WhatsApp.apk
-echo Patching AndroidManifest.xml
+echo I: Patching AndroidManifest.xml
 bin\fart.exe -q WhatsApp\AndroidManifest.xml allowBackup=\"false allowBackup=\"true
-echo Recompiling APK
+echo I: Recompiling APK
 java -jar bin\apktool.jar b WhatsApp
-echo Signing APK
+echo I: Signing APK
 java -jar bin\signapk.jar crt\wa.pem crt\wa.pk8 WhatsApp\dist\WhatsApp.apk WhatsApp_Patched.apk
-echo Patching Done!
-pause
+echo I: Patching Done!
